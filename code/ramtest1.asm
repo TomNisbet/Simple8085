adefcd766cd002acf192a2348e39d78c7e28bd44
Tom Nisbet <t.talk@nisbethome.com>
2023-10-14 00:40:21 -0400

    fix bugs in ramtest1.asm

--- code/ramtest1.asm
+++ code/ramtest1.asm
@@ -1,8 +1,12 @@
-; Test program to bit-bang a single character out SOD as serial async data
-; This program does not use any RAM or stack instructions, so it can be run
-;   from a ROM-only system.
+; Test program to bit-bang characters out SOD as serial async data
+; The character to be sent is stored in RAM.
 ; The BITTIME is calculated for a 6.144MHz processor clock.
 
+; This can be run using the simple addressing hardware where ROM OE is connected to A15
+; and RAM OE is inverted A15.  This puts ROM at 0000H and RAM at 8000H.
+
+; This can also be run on the final hardware that has RAM at 0000H and ROM at 8000H.
+; The SYSROMST and SYSRAMST must be changed to accomodate this.
 ; Note the first jump that seems to do nothing, but it clears the Reset-mode flip-flip.
 ; The ROM is normally at 8000 and the RAM at 0000.  At reset, the ROM maps to
 ; both 0000 and 8000.  This condition persists until the A15 line goes high.
@@ -10,6 +14,8 @@
 ; when it executes.  The jump to 8003 will cause the reset-mode flip-flop to
 ; clear and the normal addressing scheme will be activated.  At this point,
 ; the RAM and ROM use their normal addressing.
+; When the simple addressing is used, the initial JMP START instruction is not needed
+; but there is no harm in leaving it in.
 
 SYSROMST    equ     0000H
 SYSRAMST    equ     8000H
@@ -25,25 +31,26 @@ OUTBITS     equ     11
 START:
             mvi     a,'A'           ; Send a test character
             sta     CHRMEM
+SEND:
+            mov     c,a             ; Store the character to send in C
 COUT:
             di
-            mov     a,c             ; Get the character to send
             mvi     b,OUTBITS       ; Number of output bits
             xra     a               ; Clear carry for start bit
 CO1:
             mvi     a,080H          ; Set the SDE flag
             rar                     ; Shift carry into SOD flag
-            cmc                     ;   and invert carry.  Why? (serial is inverted?)
-            sim                     ; Output data bit
+            cmc                     ; ??? This appears to be useless, carry is  set below
+            sim                     ; Output start, data, or stop bit
             lxi     h,BITTIME       ; Load the time delay for one bit width
 CO2:
             dcr     l               ; Wait for bit time
             jnz     CO2
             dcr     h
             jnz     CO2
-            stc                     ; Shift in stop bit(s)
+            stc                     ; Shift in ones for stop bit(s)
             mov     a,c             ; Get char to send
-            rar                     ; LSB into carry
+            rar                     ; LSB of char into carry to send
             mov     c,a             ; Store rotated data
             dcr     b
             jnz     CO1             ; Send next bit
@@ -61,4 +68,4 @@ CHILL:
             jz      START           ; Reset char back to A
             inr     a               ; Advance to next char to print A thru Z
             sta     CHRMEM
-            jmp     COUT            ; Print next char
+            jmp     SEND            ; Print next char
